ecb7776abac7dc49e66889979449c4ed70736b11
Abhay Ray <abhayray@Abhays-MacBook-Air.local>
2021-09-04 17:58:26 +0530

    routets added

--- front_end/lib/main.dart
+++ front_end/lib/main.dart
@@ -1,16 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:front_end/screens/home_page.dart';
 import 'package:front_end/theme/colors/light_colors.dart';
-import 'package:flutter/services.dart';
-
-void main() {
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    systemNavigationBarColor: LightColors.kLightYellow, // navigation bar color
-    statusBarColor: Color(0xffffb969), // status bar color
-  ));
-
-  return runApp(MyApp());
-}
+import 'package:sawo/sawo.dart';
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -25,8 +15,73 @@ class MyApp extends StatelessWidget {
             displayColor: LightColors.kDarkBlue,
             fontFamily: 'Poppins'),
       ),
-      home: HomePage(),
+      home: LoginScreen(),
       debugShowCheckedModeBanner: false,
+    );
+  }
+}
+class LoginScreen extends StatefulWidget {
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+      Sawo sawo = new Sawo(
+        apiKey: "288d5890-9ea9-479c-b33d-1617f7382d0e",
+        secretKey: "613354662f5bde7b00073aa3UGSL9dIDJU168YFSUYvQg7WN",
+     );
+
+     // user payload
+  String user = "";
+
+  void payloadCallback(context, payload) {
+    if (payload == null || (payload is String && payload.length == 0)) {
+      payload = "Login Failed :(";
+    }
+    setState(() {
+      user = payload;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Sawo"),
+          centerTitle: true,
+        ),
+      body: Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("UserData :- $user"),
+            ElevatedButton(
+              onPressed: () {
+                sawo.signIn(
+                  context: context,
+                  identifierType: 'email',
+                  callback: payloadCallback,
+                );
+              },
+              child: Text('Email Login'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                sawo.signIn(
+                  context: context,
+                  identifierType: 'phone_number_sms',
+                  callback: payloadCallback,
+                );
+              },
+              child: Text('Phone Login'),
+            ),
+          ],
+        ),
+      ),
+    ),
     );
   }
 }
